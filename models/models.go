d34fa3341c88530172920f3e216e5c2234a9219d
Franklyn Ogbonna <techiefrankie@gmail.com>
2024-12-26 15:42:57 +0100

    Update models.go

--- models/models.go
+++ models/models.go
@@ -21,3 +21,35 @@ type User struct {
 	Email     string `gorm:"unique;not null;size:100"`
 	Password  string `gorm:"not null;size:255"`
 }
+
+type Registration struct {
+	Id               uint      `gorm:"primaryKey;autoIncrement"`
+	UserId           int       `gorm:"not null"`                         // Foreign key field
+	User             User      `gorm:"foreignKey:UserId;references:Id"`  // Foreign key relationship
+	EventId          uint      `gorm:"not null"`                         // Foreign key field
+	Event            Event     `gorm:"foreignKey:EventId;references:Id"` // Foreign key relationship
+	Status           Status    `gorm:"not null;size:20"`
+	EventDate        time.Time `gorm:"not null"`
+	RegistrationDate time.Time `gorm:"not null"`
+}
+
+type Status string
+
+const (
+	Completed = "COMPLETED"
+	Pending   = "PENDING"
+	Failed    = "FAILED"
+)
+
+func (s Status) String() string {
+	switch s {
+	case Completed:
+		return "COMPLETED"
+	case Pending:
+		return "PENDING"
+	case Failed:
+		return "FAILED"
+	default:
+		return "UNKNOWN"
+	}
+}
